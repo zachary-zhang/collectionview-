4ca4b9614b52bdc9ef7d42f371df80bd4d766ffb
zhnnnnn <475816908@qq.com>
2015-12-17 13:05:38 +0800

    Initial Commit

---
+++ collectionview自定义布局/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionview自定义布局
+//
+//  Created by zhn on 15/12/17.
+//  Copyright © 2015年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ collectionview自定义布局/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionview自定义布局
+//
+//  Created by zhn on 15/12/17.
+//  Copyright © 2015年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
